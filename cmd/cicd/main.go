8d197f284f5e9ac93ee93c3baad3c1be3d100b5b
Eddy Christiandy <eddy.christiandy@outlook.com>
2021-11-15 12:16:19 +0700

    wip

--- cmd/cicd/main.go
+++ cmd/cicd/main.go
@@ -10,7 +10,7 @@ import (
 func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hellos, %q", html.EscapeString(r.URL.Path))
+		fmt.Fprintf(w, "Hellow, %q", html.EscapeString(r.URL.Path))
 	})
 
 	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
